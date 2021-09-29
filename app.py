38034439e74df19128fd3348ea6a6f60ca40bd1d
marks530 <mark@ipsolutions.ie>
2021-09-29 17:15:17 +0000

    Move Register New User to admin control

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def register():
         # session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         # return redirect(url_for("profile", username=session["user"]))
+        # return user to login page after registration
         return redirect(url_for("login"))
 
     return render_template("register.html")
