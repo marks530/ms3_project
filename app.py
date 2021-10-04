4c357d241f37543d0929cc3b4c90aa0f57ccefb1
marks530 <mark@ipsolutions.ie>
2021-10-04 16:41:12 +0000

    Correct code in app.py according to PEP8 rules

--- app.py
+++ app.py
@@ -11,23 +11,32 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")  # get the database name
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI") # Mongo database connection string
-app.secret_key = os.environ.get("SECRET_KEY") # key is required when using some of the functions from Flask 
+# get the database name
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# Mongo database connection string
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# key is required when using some of the functions from Flask
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 mongo = PyMongo(app)
 
-@app.route("/") 
+
+@app.route("/")
 @app.route("/get_serviceReports")
 def get_serviceReports():
     serviceReports = list(mongo.db.serviceReports.find())
-    return render_template("serviceReports.html", serviceReports = serviceReports)
+    return render_template(
+        "serviceReports.html", serviceReports=serviceReports)
+
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
     query = request.form.get("query")
-    serviceReports = list(mongo.db.serviceReports.find({"$text": {"$search": query}}))
-    return render_template("serviceReports.html", serviceReports = serviceReports)
+    serviceReports = list(mongo.db.serviceReports.find(
+        {"$text": {"$search": query}}))
+    return render_template(
+        "serviceReports.html", serviceReports=serviceReports)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -41,7 +50,7 @@ def register():
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        confirm_password = request.form.get("confirm_password")    
+        confirm_password = request.form.get("confirm_password")
 
         register = {
             "username": request.form.get("username").lower(),
@@ -52,13 +61,12 @@ def register():
             mongo.db.users.insert_one(register)
             flash("Registration Successful!")
             # return user to login page after registration
-            return redirect(url_for("login")) 
+            return redirect(url_for("login"))
 
         else:
             flash("Passwords do not match Registration Unsuccessful!")
             # return user to register page after unsuccessful registration
     return render_template("register.html")
-
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -70,12 +78,10 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for("profile", username=session["user"]))
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -88,15 +94,16 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods=["GET","POST"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    
+
     if session["user"]:
         return render_template("profile.html", username=username)
-    
+
     return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout():
@@ -105,7 +112,8 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
-@app.route("/add_record", methods=["GET","POST"])
+
+@app.route("/add_record", methods=["GET", "POST"])
 def add_record():
     if request.method == "POST":
         is_resolved = "on" if request.form.get("is_resolved") else "off"
@@ -122,15 +130,16 @@ def add_record():
         mongo.db.serviceReports.insert_one(report)
         flash("Report Successfully Added")
         return redirect(url_for("get_serviceReports"))
-      
+
     usernames = list(mongo.db.users.find().sort("username", 1))
     return render_template("add_record.html", usernames=usernames)
+
 
 @app.route("/edit_record/<serviceReport_id>", methods=["GET", "POST"])
 def edit_record(serviceReport_id):
     if request.method == "POST":
         is_resolved = "Yes" if request.form.get("is_resolved") else "No"
-        submit= {
+        submit = {
             "engineer_name": request.form.get("engineer_name"),
             "customer_name": request.form.get("customer_name"),
             "date_received": request.form.get("date_received"),
@@ -141,12 +150,15 @@ def edit_record(serviceReport_id):
             "is_resolved": request.form.get("is_resolved")
 
         }
-        mongo.db.serviceReports.update({"_id": ObjectId(serviceReport_id)}, submit)
+        mongo.db.serviceReports.update(
+            {"_id": ObjectId(serviceReport_id)}, submit)
         flash("Record Successfully Updated")
 
-    serviceReport = mongo.db.serviceReports.find_one({"_id": ObjectId(serviceReport_id)})
+    serviceReport = mongo.db.serviceReports.find_one(
+        {"_id": ObjectId(serviceReport_id)})
     serviceReports = list(mongo.db.serviceReports.find())
-    return render_template("edit_record.html", serviceReport=serviceReport, serviceReports = serviceReports)
+    return render_template("edit_record.html", serviceReport=serviceReport, serviceReports=serviceReports)
+
 
 @app.route("/delete_record/<serviceReport_id>")
 def delete_record(serviceReport_id):
@@ -154,15 +166,18 @@ def delete_record(serviceReport_id):
     flash("Record Successfully Deleted")
     return redirect(url_for("get_serviceReports"))
 
+
 @app.route("/manage_users")
 def manage_users():
     users = list(mongo.db.users.find().sort("username", 1))
     return render_template("manage_users.html", users=users)
 
+
 @app.route("/manage_passwords")
 def manage_passwords():
     passwords = list(mongo.db.users.find().sort("password"))
     return render_template("manage_passwords.html", passwords=passwords)
+
 
 @app.route("/add_user", methods=["GET", "POST"])
 def add_user():
@@ -172,13 +187,14 @@ def add_user():
             "password": generate_password_hash(request.form.get("password")),
             "employee_type": request.form.get("employee_type")
         }
-        if request.form.get("password") == request.form.get("confirm_password"):
-            mongo.db.users.insert_one(user)
-            flash("New User Added")
-            return redirect(url_for("manage_users"))
+        if request.form.get("password") == request.form.get(
+            "confirm_password"):
+                mongo.db.users.insert_one(user)
+                flash("New User Added")
+                return redirect(url_for("manage_users"))
 
-        else: 
-            flash("Passwords do not match Add User Unsuccessful!")      
+        else:
+            flash("Passwords do not match Add User Unsuccessful!")
     return render_template("add_user.html")
 
 
@@ -187,7 +203,7 @@ def edit_user(user_id):
     if request.method == "POST":
         submit = {
             "username": request.form.get("username"),
-            "password": generate_password_hash(request.form.get ("password")),
+            "password": generate_password_hash(request.form.get("password")),
             "employee_type": request.form.get("employee_type")
         }
         mongo.db.users.update({"_id": ObjectId(user_id)}, submit)
@@ -197,13 +213,15 @@ def edit_user(user_id):
     user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
     return render_template("edit_user.html", user=user)
 
+
 @app.route("/delete_user/<user_id>")
 def delete_user(user_id):
     mongo.db.users.remove({"_id": ObjectId(user_id)})
     flash("User Removed Successfully")
     return redirect(url_for("manage_users"))
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-     port=int(os.environ.get("PORT")), 
-     debug=True)
+    port=int(os.environ.get("PORT")),
+    debug=True)
